cfbde5c9492aae7b5c03101f5cdfb43d23dd18d8
aitor <aitor@doofinder.com>
2023-08-31 13:23:45 +0200

    Add pattern matching for the currency format

--- lib/siwapp_web/views/page_view.ex
+++ lib/siwapp_web/views/page_view.ex
@@ -6,8 +6,12 @@ defmodule SiwappWeb.PageView do
   can be given. Default are [symbol: true, separator: ","]. Check Money.to_string
   to see more options available
   """
-  @spec money_format(number, atom | binary, keyword) :: binary
-  def money_format(value, currency, options \\ []) do
+  @spec money_format(number, atom | binary | nil, keyword) :: binary | nil
+  def money_format(value, currency, options \\ [])
+  
+  def money_format(_value, nil, _options), do: nil
+  
+  def money_format(value, currency, options) do
     value
     |> Money.new(currency)
     |> Money.to_string(options)
